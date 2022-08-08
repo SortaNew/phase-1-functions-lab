73622caeffe8ed0b8a4b0db044bda20ece39c836
Parry Huang <parryhuang94@gmail.com>
2022-08-08 16:53:29 -0400

    commit tree aebb1d2862ed2706e07ddad15549fea110d22fc7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(st){
+    if (st>=42)
+        return st-42;
+    else if (st<42)
+        return 42-st;
+}
+
+function distanceFromHqInFeet(st){
+    let distanceInFeet = distanceFromHqInBlocks(st)*264;
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination)
+        return (start-destination)*264;
+    else if (start < destination)
+        return (destination-start)*264; 
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance<=400)
+        return 0;
+    else if (distance>400 && distance<=2000){
+        return (distance-400)*.02;
+    }
+    else if (distance>2000 && distance<=2500){
+        return 25;
+    }
+    else if (distance>2500){
+        return `cannot travel that far`;
+    }
+}
